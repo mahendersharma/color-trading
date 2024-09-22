2748ad6b6b53680d377c562739d77bbf39556fa4
Mahender Sharma <mahender.sharma@phonato.com>
2024-09-22 20:56:07 +0530

    realtime data featch

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,46 +1,39 @@
-import axios from 'axios'
-import {removeUserFromLocalStorage} from '../utils/localStorage.js';
+import axios from 'axios';
+import { removeUserFromLocalStorage } from '../utils/localStorage.js';
 
-const BASE_URL= process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL
 
-   const dashboardAxios = axios.create({
-           baseURL:BASE_URL,
-           headers: {
-            'Content-Type': 'application/json',
-          }
-    });
+const dashboardAxios = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-store', // Prevent client-side caching
+  }
+});
 
-    dashboardAxios.interceptors.request.use((config) => {
-        const authToken = localStorage.getItem('authToken'); // Get the token as a string
-        console.log("authToken", authToken); // Log the token for debugging
-    
-        if (authToken) {
-            // Ensure headers object exists
-            if (!config.headers) {
-                config.headers = {};
-            }
-            config.headers.Authorization = `Bearer ${authToken}`;
-        }
-    
-        return config;
-    }, (error) => {
-        return Promise.reject(error);
-    });
-    
-    
+// Request Interceptor
+dashboardAxios.interceptors.request.use((config) => {
+  const authToken = localStorage.getItem('authToken');
+  console.log("authToken", authToken);
 
-    dashboardAxios.interceptors.response.use((response)=>{
-        return response
-    },(error)=>{
-        if(error.response?.status === 401){
-            removeUserFromLocalStorage()
-            window.location = '/login?message=Session Expired, Login Again'
-        }
-        else if(error.response?.status ===400){
-            removeUserFromLocalStorage()
-            window.location = '/login?message=Invalid Token, Login Again'
-        } 
-        return Promise.reject(error)
-    });
+  if (authToken) {
+    config.headers.Authorization = `Bearer ${authToken}`;
+  }
+
+  return config;
+}, (error) => Promise.reject(error));
+
+// Response Interceptor
+dashboardAxios.interceptors.response.use((response) => response, (error) => {
+  if (error.response?.status === 401) {
+    removeUserFromLocalStorage();
+    window.location = '/login?message=Session Expired, Login Again';
+  } else if (error.response?.status === 400) {
+    removeUserFromLocalStorage();
+    window.location = '/login?message=Invalid Token, Login Again';
+  }
+
+  return Promise.reject(error);
+});
 
 export default dashboardAxios;
